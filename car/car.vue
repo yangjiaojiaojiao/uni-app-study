3fde64fc4a46a86beb47b6396a2be73814253484
杨娇 <33597901+yangjiaojiaojiao@users.noreply.github.com>
2020-07-15 10:35:18 +0800

    Add files via upload

---
+++ car/car.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		我的购物车是空的
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
